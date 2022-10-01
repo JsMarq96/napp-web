25f814b15cf07d87ca46f2da3dbd8949d527e76a
Juan S. Marquerie <juansebastian.marquerie01@estudiant.upf.edu>
2022-10-01 19:01:34 +0200

    Fixed IBL

--- js/shaders/block_shaders.js
+++ js/shaders/block_shaders.js
@@ -230,11 +230,11 @@ vec3 get_pbr_color(const in sFragData data, const in sFragVects vects) {
 vec3 get_IBL_contribution(const in sFragData data, const in sFragVects vects) {
     vec2 LUT_brdf = texture(u_brdf_LUT, vec2(vects.n_dot_v, data.roughness)).rg;
     vec3 fresnel_IBL = fresnel_schlick(vects.n_dot_v, data.f0, data.roughness);
-    vec3 specular_sample = linear_to_gamma(texture(u_enviorment_map, vects.r, data.roughness).rgb);
+    vec3 specular_sample = linear_to_gamma(texture(u_enviorment_map, vects.r, 5.0 * data.roughness).rgb);
 
     vec3 specular_IBL = ((fresnel_IBL * LUT_brdf.x) + LUT_brdf.y) * specular_sample;
 
-	vec3 diffuse_IBL = data.albedo * linear_to_gamma(texture(u_enviorment_map, -data.normal, 0.5).rgb) * (1.0 - fresnel_IBL);
+	vec3 diffuse_IBL = data.albedo * linear_to_gamma(texture(u_enviorment_map, vects.r, 8.0).rgb) * (1.0 - fresnel_IBL);
 
 	//return vec3(0.0);
 	//return (specular) + diffuse;
